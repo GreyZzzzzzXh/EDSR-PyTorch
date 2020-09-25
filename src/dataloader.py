78692070246c6b8c4b6b923fca395bb815104f5c
GreyZzzzzzXh <zhangxueheng@sjtu.edu.cn>
2020-09-26 01:34:56 +0800

    MDSR PyTorch 1.5.1

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,37 +1,76 @@
 import threading
 import random
+import itertools
 
 import torch
 import torch.multiprocessing as multiprocessing
-from torch.utils.data import DataLoader
-from torch.utils.data import SequentialSampler
-from torch.utils.data import RandomSampler
-from torch.utils.data import BatchSampler
-from torch.utils.data import _utils
-from torch.utils.data.dataloader import _DataLoaderIter
 
-from torch.utils.data._utils import collate
+from torch.utils.data import DataLoader
+from torch.utils.data import _utils
+from torch.utils.data.dataloader import _MultiProcessingDataLoaderIter
 from torch.utils.data._utils import signal_handling
 from torch.utils.data._utils import MP_STATUS_CHECK_INTERVAL
-from torch.utils.data._utils import ExceptionWrapper
-from torch.utils.data._utils import IS_WINDOWS
 from torch.utils.data._utils.worker import ManagerWatchdog
+from torch.utils.data._utils.worker import _worker_info
+from torch.utils.data._utils.worker import WorkerInfo
+from torch.utils.data._utils.worker import _IterableDatasetStopIteration
 
 from torch._six import queue
+from torch._utils import ExceptionWrapper
 
-def _ms_loop(dataset, index_queue, data_queue, done_event, collate_fn, scale, seed, init_fn, worker_id):
+
+def _ms_loop(dataset_kind, dataset, index_queue, data_queue, done_event,
+             auto_collation, collate_fn, drop_last, seed, init_fn, worker_id,
+             num_workers, scale):
+    # See NOTE [ Data Loader Multiprocessing Shutdown Logic ] for details on the
+    # logic of this function.
+
     try:
-        collate._use_shared_memory = True
+        # Initialize C side signal handlers for SIGBUS and SIGSEGV. Python signal
+        # module's handlers are executed after Python returns from C low-level
+        # handlers, likely when the same fatal signal had already happened
+        # again.
+        # https://docs.python.org/3/library/signal.html#execution-of-python-signal-handlers
         signal_handling._set_worker_signal_handlers()
 
         torch.set_num_threads(1)
         random.seed(seed)
         torch.manual_seed(seed)
 
-        data_queue.cancel_join_thread()
+        global _worker_info
+        _worker_info = WorkerInfo(id=worker_id,
+                                  num_workers=num_workers,
+                                  seed=seed,
+                                  dataset=dataset)
 
-        if init_fn is not None:
-            init_fn(worker_id)
+        from torch.utils.data import _DatasetKind
+
+        init_exception = None
+
+        try:
+            if init_fn is not None:
+                init_fn(worker_id)
+
+            fetcher = _DatasetKind.create_fetcher(dataset_kind, dataset,
+                                                  auto_collation, collate_fn,
+                                                  drop_last)
+        except Exception:
+            init_exception = ExceptionWrapper(
+                where="in DataLoader worker process {}".format(worker_id))
+
+        # When using Iterable mode, some worker can exit earlier than others due
+        # to the IterableDataset behaving differently for different workers.
+        # When such things happen, an `_IterableDatasetStopIteration` object is
+        # sent over to the main process with the ID of this worker, so that the
+        # main process won't send more tasks to this worker, and will send
+        # `None` to this worker to properly exit it.
+        #
+        # Note that we cannot set `done_event` from a worker as it is shared
+        # among all processes. Instead, we set the `iteration_end` flag to
+        # signify that the iterator is exhausted. When either `done_event` or
+        # `iteration_end` is set, we skip all processing step and just wait for
+        # `None`.
+        iteration_end = False
 
         watchdog = ManagerWatchdog()
 
@@ -40,119 +79,143 @@ def _ms_loop(dataset, index_queue, data_queue, done_event, collate_fn, scale, se
                 r = index_queue.get(timeout=MP_STATUS_CHECK_INTERVAL)
             except queue.Empty:
                 continue
-
             if r is None:
-                assert done_event.is_set()
-                return
-            elif done_event.is_set():
+                # Received the final signal
+                assert done_event.is_set() or iteration_end
+                break
+            elif done_event.is_set() or iteration_end:
+                # `done_event` is set. But I haven't received the final signal
+                # (None) yet. I will keep continuing until get it, and skip the
+                # processing steps.
                 continue
-
-            idx, batch_indices = r
-            try:
-                idx_scale = 0
-                if len(scale) > 1 and dataset.train:
-                    idx_scale = random.randrange(0, len(scale))
-                    dataset.set_scale(idx_scale)
-
-                samples = collate_fn([dataset[i] for i in batch_indices])
-                samples.append(idx_scale)
-            except Exception:
-                data_queue.put((idx, ExceptionWrapper(sys.exc_info())))
+            idx, index = r
+            if init_exception is not None:
+                data = init_exception
+                init_exception = None
             else:
-                data_queue.put((idx, samples))
-                del samples
+                try:
+                    idx_scale = 0
+                    if len(scale) > 1 and dataset.train:
+                        idx_scale = random.randrange(0, len(scale))
+                        fetcher.dataset.set_scale(idx_scale)
 
+                    data = fetcher.fetch(index)
+                    data.append(idx_scale)
+
+                except Exception as e:
+                    if isinstance(e, StopIteration
+                                  ) and dataset_kind == _DatasetKind.Iterable:
+                        data = _IterableDatasetStopIteration(worker_id)
+                        # Set `iteration_end`
+                        #   (1) to save future `next(...)` calls, and
+                        #   (2) to avoid sending multiple `_IterableDatasetStopIteration`s.
+                        iteration_end = True
+                    else:
+                        # It is important that we don't store exc_info in a variable.
+                        # `ExceptionWrapper` does the correct thing.
+                        # See NOTE [ Python Traceback Reference Cycle Problem ]
+                        data = ExceptionWrapper(
+                            where="in DataLoader worker process {}".format(
+                                worker_id))
+            data_queue.put((idx, data))
+            del data, idx, index, r  # save memory
     except KeyboardInterrupt:
+        # Main process will raise KeyboardInterrupt anyways.
         pass
+    if done_event.is_set():
+        data_queue.cancel_join_thread()
+        data_queue.close()
 
-class _MSDataLoaderIter(_DataLoaderIter):
 
+class _MSDataLoaderIter(_MultiProcessingDataLoaderIter):
     def __init__(self, loader):
-        self.dataset = loader.dataset
+        super(_MultiProcessingDataLoaderIter, self).__init__(loader)
+
+        assert self._num_workers > 0
+
         self.scale = loader.scale
-        self.collate_fn = loader.collate_fn
-        self.batch_sampler = loader.batch_sampler
-        self.num_workers = loader.num_workers
-        self.pin_memory = loader.pin_memory and torch.cuda.is_available()
-        self.timeout = loader.timeout
 
-        self.sample_iter = iter(self.batch_sampler)
+        if loader.multiprocessing_context is None:
+            multiprocessing_context = multiprocessing
+        else:
+            multiprocessing_context = loader.multiprocessing_context
 
-        base_seed = torch.LongTensor(1).random_().item()
+        self._worker_init_fn = loader.worker_init_fn
+        self._worker_queue_idx_cycle = itertools.cycle(range(
+            self._num_workers))
+        self._worker_result_queue = multiprocessing_context.Queue()
+        self._worker_pids_set = False
+        self._shutdown = False
+        self._send_idx = 0  # idx of the next task to be sent to workers
+        self._rcvd_idx = 0  # idx of the next task to be returned in __next__
+        # information about data not yet yielded, i.e., tasks w/ indices in range [rcvd_idx, send_idx).
+        # map: task idx => - (worker_id,)        if data isn't fetched (outstanding)
+        #                  \ (worker_id, data)   if data is already fetched (out-of-order)
+        self._task_info = {}
+        self._tasks_outstanding = 0  # always equal to count(v for v in task_info.values() if len(v) == 1)
+        self._workers_done_event = multiprocessing_context.Event()
 
-        if self.num_workers > 0:
-            self.worker_init_fn = loader.worker_init_fn
-            self.worker_queue_idx = 0
-            self.worker_result_queue = multiprocessing.Queue()
-            self.batches_outstanding = 0
-            self.worker_pids_set = False
-            self.shutdown = False
-            self.send_idx = 0
-            self.rcvd_idx = 0
-            self.reorder_dict = {}
-            self.done_event = multiprocessing.Event()
+        self._index_queues = []
+        self._workers = []
+        # A list of booleans representing whether each worker still has work to
+        # do, i.e., not having exhausted its iterable dataset object. It always
+        # contains all `True`s if not using an iterable-style dataset
+        # (i.e., if kind != Iterable).
+        self._workers_status = []
+        for i in range(self._num_workers):
+            index_queue = multiprocessing_context.Queue()
+            # index_queue.cancel_join_thread()
+            w = multiprocessing_context.Process(
+                target=_ms_loop,
+                args=(self._dataset_kind, self._dataset, index_queue,
+                      self._worker_result_queue, self._workers_done_event,
+                      self._auto_collation, self._collate_fn, self._drop_last,
+                      self._base_seed + i, self._worker_init_fn, i,
+                      self._num_workers, self.scale))
+            w.daemon = True
+            # NB: Process.start() actually take some time as it needs to
+            #     start a process and pass the arguments over via a pipe.
+            #     Therefore, we only add a worker to self._workers list after
+            #     it started, so that we do not call .join() if program dies
+            #     before it starts, and __del__ tries to join but will get:
+            #     AssertionError: can only join a started process.
+            w.start()
+            self._index_queues.append(index_queue)
+            self._workers.append(w)
+            self._workers_status.append(True)
 
-            base_seed = torch.LongTensor(1).random_()[0]
+        if self._pin_memory:
+            self._pin_memory_thread_done_event = threading.Event()
+            self._data_queue = queue.Queue()
+            pin_memory_thread = threading.Thread(
+                target=_utils.pin_memory._pin_memory_loop,
+                args=(self._worker_result_queue, self._data_queue,
+                      torch.cuda.current_device(),
+                      self._pin_memory_thread_done_event))
+            pin_memory_thread.daemon = True
+            pin_memory_thread.start()
+            # Similar to workers (see comment above), we only register
+            # pin_memory_thread once it is started.
+            self._pin_memory_thread = pin_memory_thread
+        else:
+            self._data_queue = self._worker_result_queue
 
-            self.index_queues = []
-            self.workers = []
-            for i in range(self.num_workers):
-                index_queue = multiprocessing.Queue()
-                index_queue.cancel_join_thread()
-                w = multiprocessing.Process(
-                    target=_ms_loop,
-                    args=(
-                        self.dataset,
-                        index_queue,
-                        self.worker_result_queue,
-                        self.done_event,
-                        self.collate_fn,
-                        self.scale,
-                        base_seed + i,
-                        self.worker_init_fn,
-                        i
-                    )
-                )
-                w.daemon = True
-                w.start()
-                self.index_queues.append(index_queue)
-                self.workers.append(w)
+        _utils.signal_handling._set_worker_pids(
+            id(self), tuple(w.pid for w in self._workers))
+        _utils.signal_handling._set_SIGCHLD_handler()
+        self._worker_pids_set = True
 
-            if self.pin_memory:
-                self.data_queue = queue.Queue()
-                pin_memory_thread = threading.Thread(
-                    target=_utils.pin_memory._pin_memory_loop,
-                    args=(
-                        self.worker_result_queue,
-                        self.data_queue,
-                        torch.cuda.current_device(),
-                        self.done_event
-                    )
-                )
-                pin_memory_thread.daemon = True
-                pin_memory_thread.start()
-                self.pin_memory_thread = pin_memory_thread
-            else:
-                self.data_queue = self.worker_result_queue
-
-            _utils.signal_handling._set_worker_pids(
-                id(self), tuple(w.pid for w in self.workers)
-            )
-            _utils.signal_handling._set_SIGCHLD_handler()
-            self.worker_pids_set = True
-
-            for _ in range(2 * self.num_workers):
-                self._put_indices()
+        # prime the prefetch loop
+        for _ in range(2 * self._num_workers):
+            self._try_put_index()
 
 
 class MSDataLoader(DataLoader):
-
     def __init__(self, cfg, *args, **kwargs):
-        super(MSDataLoader, self).__init__(
-            *args, **kwargs, num_workers=cfg.n_threads
-        )
+        super(MSDataLoader, self).__init__(*args,
+                                           **kwargs,
+                                           num_workers=cfg.n_threads)
         self.scale = cfg.scale
 
     def __iter__(self):
         return _MSDataLoaderIter(self)
-
